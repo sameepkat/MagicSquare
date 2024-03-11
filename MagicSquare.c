28ca3d2aa5cf2622f2e034ca732a2ce005f65e26
[sameepkat] <[sameepsk2@gmail.com]>
2024-03-11 10:04:44 +0545

    gitignore and minor adjustments

--- MagicSquare.c
+++ MagicSquare.c
@@ -5,7 +5,13 @@ int main()
 		int n;
 		printf("Enter the order of magic square: ");
 		scanf("%d", &n);
-		square(n);
+		if(n<3)
+			printf("\aMagic Square of order less than 3 is not possible\n");
+		else
+		{
+			printf("MAGIC SQUARE OF %dx%d:\n",n,n);
+			square(n);
+		}
 	return(0);
 }
 
--- SquareGenerator.h
+++ SquareGenerator.h
@@ -3,12 +3,13 @@ void square(int x)
 	int i,j,a=1;
 	for(i=1;i<=x;i++)
 	{
+		printf(" |\t");
 		for(j=1;j<=x;j++)
 		{
 			printf("%2d\t",a);
 			a++;
 		}
-		printf("\n");
+		printf(" |\n");
 	}
 
 		
