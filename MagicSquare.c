3f2376994c9aa7f932fc8013da25761b00e6932a
[sameepkat] <[sameepsk2@gmail.com]>
2024-03-11 09:20:34 +0545

    Initial Commit

---
+++ MagicSquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"SquareGenerator.h"
+int main()
+	{
+		int n;
+		printf("Enter the order of magic square: ");
+		scanf("%d", &n);
+		square(n);
+	return(0);
+}
+
---
+++ SquareGenerator.h
@@ -0,0 +1,15 @@
+void square(int x)
+{
+	int i,j,a=1;
+	for(i=1;i<=x;i++)
+	{
+		for(j=1;j<=x;j++)
+		{
+			printf("%2d\t",a);
+			a++;
+		}
+		printf("\n");
+	}
+
+		
+}
